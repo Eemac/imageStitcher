e1c069ae2d6dbcd09c56c475dec3a18112d17d0b
AriB2003 <ari.bobesh@gmail.com>
2024-12-06 12:06:26 -0500

    Add circular support

--- get_corners.m
+++ get_corners.m
@@ -1,8 +1,10 @@
 function [strongest_left, strongest_right, quantity_l, quantity_r, combined] = get_corners(left, right, quantity_l, quantity_r, suppress)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    corners_left = detectHarrisFeatures(im2gray(left),"MinQuality",0);
-    corners_right = detectHarrisFeatures(im2gray(right),"MinQuality",0);
+    gray_left = im2gray(left);
+    gray_right = im2gray(right);
+    corners_left = detectHarrisFeatures(gray_left,"MinQuality",0);
+    corners_right = detectHarrisFeatures(gray_right,"MinQuality",0);
     combined = zeros(size(left));
     combined = [combined combined];
     combined(1:size(left,1),1:size(left,2),1:size(left,3)) = left;
@@ -13,10 +15,20 @@ function [strongest_left, strongest_right, quantity_l, quantity_r, combined] = g
         imshow(combined);
         hold on;
     end
+    
+    thresh=5;
+    offset=5;
+    left_mask = ~logical(ceil(conv2(uint8(gray_left<=thresh),ones(offset),"same")));
+    right_mask = ~logical(ceil(conv2(uint8(gray_right<=thresh),ones(offset),"same")));
+
     strongest_left = corners_left.selectStrongest(quantity_l);
     strongest_right = corners_right.selectStrongest(quantity_r);
     strongest_left.Location = fliplr(strongest_left.Location);
     strongest_right.Location = fliplr(strongest_right.Location);
+
+    strongest_left = strongest_left(left_mask(sub2ind(size(left_mask),round(strongest_left.Location(:,1)),round(strongest_left.Location(:,2)))));
+    strongest_right = strongest_right(right_mask(sub2ind(size(right_mask),round(strongest_right.Location(:,1)),round(strongest_right.Location(:,2)))));
+
     quantity_l = min(quantity_l,length(strongest_left));
     quantity_r = min(quantity_r,length(strongest_right));
     quantity_l = min(quantity_l, quantity_r);
--- merge_images.m
+++ merge_images.m
@@ -47,12 +47,14 @@ function [out, valid] = merge_images(im1, im2, dx, dy)
     %add image 1
     out(originy_im1:originy_im1+size(im1,1)-1,originx_im1:originx_im1+size(im1,2)-1, :) = im1(:,:,:);
     out1(originy_im1:originy_im1+size(im1,1)-1,originx_im1:originx_im1+size(im1,2)-1, :) = im1(:,:,:);
+    out1_gray = im2gray(out1);
 
     %add image 2
     out(originy_im2:originy_im2+size(im2,1)-1,originx_im2:originx_im2+size(im2,2)-1, :) = im2(:,:,:);
     out2(originy_im2:originy_im2+size(im2,1)-1,originx_im2:originx_im2+size(im2,2)-1, :) = im2(:,:,:);
+    out2_gray = im2gray(out2);
 
-    overlap = and(im2gray(out1)~=0,im2gray(out2)~=0);
+    overlap = and(out1_gray~=0,out2_gray~=0);
     difference = sum(abs(int8(out1(overlap))-int8(out2(overlap))),"all");
     thresh = 25*sum(overlap,"all");
     difference/thresh
@@ -60,11 +62,12 @@ function [out, valid] = merge_images(im1, im2, dx, dy)
 
     if ~valid
         out = im1;
+    else
+        % out = max(out1,out2);
+        out3 = out1.*uint8(out2_gray==0); % image 1 but not 2
+        out4 = out2.*uint8(out1_gray==0); % image 2 but not 1
+        out5 = out1.*uint8(out1_gray~=0).*uint8(out2_gray~=0); % images 1 and 2
+        out = out3+out4+out5;
     end
-
-    % out3=out2.*uint8(out1==0); % 2 but not 1
-    % out4=out1.*uint8(out1~=0).*uint8(out2~=0); % 1 and 2
-    % out1=out1.*uint8(out2==0); % 1 but not 2
-    % out=out1+out3+out4;
 
 end
